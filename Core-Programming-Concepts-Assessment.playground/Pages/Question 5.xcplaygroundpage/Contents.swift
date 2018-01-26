7062c9f4d0636714de21606e319e3bf232bc25c3
Zhusupbekova, May <mzhusupbekova@lcs-mba3239.lcs.on.ca>
2018-01-26 15:25:35 -0500

    I can write concise statements using Boolean operators or pattern matching to check for multiple conditions. ex 5

--- Core-Programming-Concepts-Assessment.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
+++ Core-Programming-Concepts-Assessment.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
@@ -24,7 +24,11 @@ let temperatureInCelsius = -6
 let isSnowMakingEquipmentBroken = true
 
 // Answer question 5 below
-
+if newSnowfallAmount >= 10 && temperatureInCelsius <= -5 && isSnowMakingEquipmentBroken == false {
+    print("We're open! ")
+} else {
+print ("We're closed")
+}
 
 /*:
  ## Now share your understanding
